9d066af233f36a78085507a3b2755d0bb8b7fa91
smd <88483238+x8vxv8x@users.noreply.github.com>
2026-01-30 17:10:55 +0800

    1

--- src/main/java/com/smd/worseperformance/WorsePerformance.java
+++ src/main/java/com/smd/worseperformance/WorsePerformance.java
@@ -1,13 +1,12 @@
-package com.example.modid;
+package com.smd.worseperformance;
 
-import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class WorsePerformance {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
